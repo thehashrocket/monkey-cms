f9707344a1ece022145d22493cbb1e12761eccc8
Jason Shultz <jshultz@axis41.com>
2012-08-30 09:24:16 -0600

    added routes.php to mix

---
+++ application/cache/routes.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$route["index"] = "pages/index/1/1";
+$route["subpagetest"] = "pages/index/1/4";
+$route["About Us"] = "pages/index/1/3";
