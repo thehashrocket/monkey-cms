b286ee1441737127eb8fccde086929b353ae4a7f
Jason Shultz <jason@openskymedia.com>
2012-09-07 10:16:54 -0600

    Update application/config/database.php

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'develop';
 $active_record = TRUE;
 
-$db['live']['hostname'] = 'mysql-shared-02.phpfog.com';
-$db['live']['username'] = 'jason-96-55499';
-$db['live']['password'] = 'Fj35J03j94iq';
-$db['live']['database'] = 'testosm_phpfogapp_com';
+$db['live']['hostname'] = '';
+$db['live']['username'] = '';
+$db['live']['password'] = '';
+$db['live']['database'] = '';
 $db['live']['dbdriver'] = 'mysql';
 $db['live']['dbprefix'] = '';
 $db['live']['pconnect'] = TRUE;
@@ -65,10 +65,10 @@ $db['live']['autoinit'] = TRUE;
 $db['live']['stricton'] = FALSE;
 
 
-$db['develop']['hostname'] = 'localhost';
-$db['develop']['username'] = 'axis41';
+$db['develop']['hostname'] = '';
+$db['develop']['username'] = '';
 $db['develop']['password'] = '';
-$db['develop']['database'] = 'monkeydb';
+$db['develop']['database'] = '';
 $db['develop']['dbdriver'] = 'mysql';
 $db['develop']['dbprefix'] = '';
 $db['develop']['pconnect'] = TRUE;
