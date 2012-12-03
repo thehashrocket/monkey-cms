54bc18d1b195be71b27927c8bf86724f53d39dbd
Jason Shultz <jasshultz@gmail.com>
2012-12-02 20:36:26 -0700

    working on routes

--- application/cache/routes.php
+++ application/cache/routes.php
@@ -1,4 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $route["home"] = "pages/index/1/1";
+$route["a_new_name"] = "pages/index/1/60";
 $route["test_page"] = "pages/index/1/59";
 $route["about_us"] = "pages/index/1/3";
