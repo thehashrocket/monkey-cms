60dab20a772a5c55fd37c3b2b6b4ee7b1cca5f3f
Jason Shultz <jasshultz@gmail.com>
2012-12-04 20:40:29 -0700

    changed db settings

---
+++ application/logs/log-2012-12-04.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-12-04 20:34:14 --> 404 Page Not Found --> favicon.ico
+ERROR - 2012-12-04 20:34:14 --> 404 Page Not Found --> favicon.ico
+ERROR - 2012-12-04 20:34:14 --> 404 Page Not Found --> favicon.ico
+ERROR - 2012-12-04 20:35:05 --> 404 Page Not Found --> favicon.ico
+ERROR - 2012-12-04 20:35:06 --> 404 Page Not Found --> favicon.ico
+ERROR - 2012-12-04 20:35:09 --> 404 Page Not Found --> favicon.ico
